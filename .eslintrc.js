ce4f3e972db63e2a5c34e9cccf6f442d0a124002
Anatoly Semenov <anatolysrmenov@gmail.com>
2024-08-18 19:42:23 +0300

    feat: install dependency @trivago/prettier-plugin-sort-imports and update prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,43 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
+    project: "tsconfig.json",
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ["@typescript-eslint/eslint-plugin"],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [".eslintrc.js"],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+    "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+    semi: ["error", "never"],
+    "linebreak-style": "off",
+    "require-jsdoc": "off",
+    "no-console": "off",
+    "no-tabs": "off",
+    camelcase: "off",
+    indent: "off",
+
+    // ts
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/quotes": "off"
+  }
+}
